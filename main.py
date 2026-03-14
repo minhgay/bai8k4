e7698d7fc183fde645b950254fc57bec4119f9e3
minhgay <145848565+minhgay@users.noreply.github.com>
2026-03-14 18:04:34 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+import streamlit as st 
+st.image('https://www.google.com/imgres?q=photo&imgurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1508921912186-1d1a45ebb3c1%3Ffm%3Djpg%26q%3D60%26w%3D3000%26auto%3Dformat%26fit%3Dcrop%26ixlib%3Drb-4.1.0%26ixid%3DM3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGhvdG98ZW58MHx8MHx8fDA%253D&imgrefurl=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fphoto&docid=AgAT_yR1X6xrJM&tbnid=-gICFkUsIVmdYM&vet=12ahUKEwiko5u2np-TAxWaklYBHeozA40QnPAOegQIGhAB..i&w=3000&h=4500&hcb=2&ved=2ahUKEwiko5u2np-TAxWaklYBHeozA40QnPAOegQIGhAB',caption='US-Angriffe auf iranische Öl-Insel Kharg
+')
